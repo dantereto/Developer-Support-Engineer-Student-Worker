b20aba0228c3ed0e4eaf622ba6b0401d63fa2eaf
dantereto <2247@holbertonschool.com>
2022-12-05 17:23:14 -0500

    First Challenges

---
+++ Plane Programming/Planes/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+    offset = transform.position - plane.transform.position;
+    }
+    void Update()
+    {
+    transform.position = plane.transform.position + offset;
+    }
+}
